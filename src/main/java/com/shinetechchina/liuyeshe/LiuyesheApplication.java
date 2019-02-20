4553b4af086a1fecc9159a419d9b62e6e19a05e8
刘建 <864282457@qq.com>
2019-02-20 17:36:49 +0800

    ..

---
+++ src/main/java/com/shinetechchina/liuyeshe/LiuyesheApplication.java
@@ -0,0 +1,13 @@
+package com.shinetechchina.liuyeshe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuyesheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiuyesheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinetechchina/liuyeshe/LiuyesheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shinetechchina.liuyeshe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiuyesheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
